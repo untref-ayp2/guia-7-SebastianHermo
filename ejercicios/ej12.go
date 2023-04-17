cda735ebdba374d9a8ba8ca06b3c284b4fad47b0
SebastianHermo <sebastianhermoavila@gmail.comm>
2023-04-17 16:36:41 -0300

    ejercicios terminados

--- ejercicios/ej12.go
+++ ejercicios/ej12.go
@@ -2,5 +2,11 @@ package ejercicios
 
 // Escriba un método recursivo que calcule Fibonacci de n.
 func Fibonacci(n int) int {
-	panic("Not implemented")
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return Fibonacci(n-1) + Fibonacci(n-2)
 }
